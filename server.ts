1c0433d69a82b9ac85cb686ddeb4c3238945475b
patttan-kku <patttan@kku.ac.th>
2026-08-11 09:23:20 +0700

    chore: update default server port to 3003

--- server.ts
+++ server.ts
@@ -286,7 +286,7 @@ const MOCK_USERS = [
 async function startServer() {
   await connectToMongo();
   const app = express();
-  const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+  const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3003;
 
   app.use(cors());
   app.use(express.json());
